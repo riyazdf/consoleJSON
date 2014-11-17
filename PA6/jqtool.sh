d664cd3f28b32891dc21d34e34fed4421374ff4a
Riyaz Faizullabhoy <riyazdf@berkeley.edu>
2014-11-16 23:18:07 -0800

    even more jq

--- PA6/jqtool.sh
+++ PA6/jqtool.sh
@@ -32,3 +32,18 @@ echo $nnested | jq '{foo, bar: {boo: (.bar.boo), baz: {foofoo: (.bar.baz.foofoo)
 # This time we'll just search for the foofoo key using jq filter composition
 echo $nnested
 echo $nnested | jq '.bar? | .baz? | .foofoo?'
+
+# Now let's play with some map()
+# This will output the values of the key foo, if it exists in the JSON
+# (foo exists for values 1, 3, 5)
+arr='[{"foo": 1}, {"no-foo": 2}, {"foo": 3}, {"no-foo":4}, {"foo":5}]'
+echo $arr
+echo $arr | jq 'map(.foo?)'
+
+# Now, we will compose map with another filter to increment,
+# And a reduction to sum the resulting values
+# Note that we need the select() because when the mapper fails to find foo, it will 
+# Output an empty value that will actually be incremented if we don't unselect it (this is weird)
+arr='[{"foo": 1}, {"no-foo": 2}, {"foo": 3}, {"no-foo":4}, {"foo":5}]'
+echo $arr
+echo $arr | jq 'map(.foo?) | map(select(.>=1)) | map(.+1) | add'
