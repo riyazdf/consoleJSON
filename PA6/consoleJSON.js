12c56d5f991bd163cdb126af79da055809d589d7
Raymond Shiau <raymond.shiau@sbcglobal.net>
2014-11-16 23:43:09 -0800

    add more functions to consoleJSON.js

--- PA6/consoleJSON.js
+++ PA6/consoleJSON.js
@@ -1,6 +1,22 @@
 var consoleJSON = consoleJSON || {};
 
 consoleJSON.log = function(json, ruleset) {
+  // pretty prints JSON to console according to given ruleset
   // obj is a Javascript object, ruleset is a consoleJSON ruleset
-  console.log(json);
+  console.traverse(json, ruleset, 0);
+  //console.log(json);
+};
+
+consoleJSON.traverse = function(json, ruleset, lvl) {
+  // traverses the json tree
+  // lvl is the depth of the current node (for indentation)
+  consoleJSON.printBegin(json, ruleset, lvl);
+
+  consoleJSON.printEnd(json, ruleset, lvl);
+};
+
+consoleJSON.printBegin = function(json, ruleset, lvl) {
+};
+
+consoleJSON.printEnd = function(json, ruleset, lvl) {
 };
