8e2558220c31574f6f1f2ae493ac7610880d3b9e
Andrew Fang <andrewbrianfang@gmail.com>
2014-11-18 14:33:54 -0800

    I hear you like brita, so here's some filtering

--- PA6/consoleJSON.js
+++ PA6/consoleJSON.js
@@ -1,4 +1,5 @@
 var consoleJSON = consoleJSON || {};
+var ruleset = ruleset || {};
 
 var DELIMITER = "  ";
 var LINE_LENGTH = 80;
@@ -66,33 +67,39 @@ consoleJSON.traverseArray = function(jsonArray, ruleset, lvl) {
 };
 
 consoleJSON.traverseObject = function(jsonObj, ruleset, lvl) {
+  var ruleset = ruleset || {};
   var keys = Object.keys(jsonObj);
   for (var i = 0; i < keys.length; i++) {
     var key = keys[i];
     var keyOutput = consoleJSON.outputKey(key, ruleset);
     var val = jsonObj[key];
-    var type = $.type(val);
-    switch (type) {
-      case 'array':
-      case 'object':
-        var beginD = consoleJSON.beginDelimiter(val, ruleset);
-        consoleJSON.startGroup(keyOutput + ": " + beginD, lvl, DELIMITER);
-    
-        consoleJSON.traverse(val, ruleset, lvl+1);
-        
-        var endD = consoleJSON.endDelimiter(val, ruleset);
-        if (i < keys.length-1) {
-          endD = endD + ",";
-        }
-        consoleJSON.print(endD, lvl , DELIMITER);
-        consoleJSON.endGroup();
-        break;
-      default:
-        var output = consoleJSON.outputVal(val, ruleset);
-        if (i < keys.length-1) {
-          output = output + ",";
-        }
-        consoleJSON.print(keyOutput + ": " + output, lvl, DELIMITER);
+    var valType = $.type(val);
+    if (!('filter' in ruleset) || ('filter' in ruleset && key == ruleset['filter'])) {
+      switch (valType) {
+        case 'array':
+        case 'object':
+          delete ruleset['filter']
+          var beginD = consoleJSON.beginDelimiter(val, ruleset);
+          consoleJSON.startGroup(keyOutput + ": " + beginD, lvl, DELIMITER);
+      
+          consoleJSON.traverse(val, ruleset, lvl+1);
+          
+          var endD = consoleJSON.endDelimiter(val, ruleset);
+          if (i < keys.length-1) {
+            endD = endD + ",";
+          }
+          consoleJSON.print(endD, lvl , DELIMITER);
+          consoleJSON.endGroup();
+          break;
+        default:
+          var output = consoleJSON.outputVal(val, ruleset);
+          if (i < keys.length-1) {
+            output = output + ",";
+          }
+          consoleJSON.print(keyOutput + ": " + output, lvl, DELIMITER);
+      }
+    } else if (valType == 'array' || valType == 'object') {
+      consoleJSON.traverse(val, ruleset, lvl);
     }
   }
 };
@@ -172,8 +179,14 @@ consoleJSON.endGroup = function() {
   console.groupEnd();
 };
 
+consoleJSON.filter = function(json, filterKey) {
+  ruleset['filter'] = filterKey;
+  consoleJSON.log(json, ruleset);
+  delete ruleset['filter'];
+}
 // From http://stackoverflow.com/questions/202605/repeat-string-javascript
 String.prototype.repeat = function(num) {
   return new Array(num+1).join(this);
 };
+
 
