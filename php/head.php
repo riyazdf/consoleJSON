3496fcd99f67841c94133a6a0b08db0798271624
Andrew Fang <andrewbrianfang@gmail.com>
2014-11-27 11:13:34 -0800

    Enable multi-pages for php

---
+++ php/head.php
@@ -0,0 +1,13 @@
+<?php
+echo'
+<meta name="author" content="Riyaz Faizullabhoy">
+<meta name="author" content="Andrew Fang">
+<meta name="author" content="Raymond Shiau">
+
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+<link rel="icon" href="../assets/favicon.png"></script>
+<script src="../jquery.js"> </script>
+<link rel="stylesheet" type="text/css" href="../style.css"></link>
+<script src="../script.js"></script>
+';
+?>
---
+++ php/menu.php
@@ -0,0 +1,15 @@
+<?php
+echo'
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="container-fluid">
+  <ul class="nav navbar-nav navbar-left">
+    <li><a href="/home/" id="homeBtn">Home</a></li>
+    <li><a href="/docs/" id="docBtn">Documentation</a></li>
+    <li><a href="https://www.github.com/riyazdf/consolejson">Github</a></li>
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+    <li><a href="/about/" id="aboutBtn">About</a></li>
+  </ul>
+</div>
+</nav>';
+?>
