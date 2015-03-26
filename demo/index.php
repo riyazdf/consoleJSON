428a9d25fef3e5cd4af3462ce33f02572dabd077
Riyaz Faizullabhoy <riyazdf@berkeley.edu>
2015-03-25 22:17:13 -0700

    add more examples to demo page

--- demo/index.php
+++ demo/index.php
@@ -1,1 +1,12 @@
-<?php include_once("demo.html"); ?>
+<?php
+switch($_GET['type']) {
+  case "defaults":
+    include_once('demo-defaults.html');
+    break;
+  case "discovery":
+    include_once('demo-discovery.html');
+    break;
+  default:
+    include_once('demo.html');
+}
+?>
